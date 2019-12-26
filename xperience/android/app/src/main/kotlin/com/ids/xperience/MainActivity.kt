1dfe2aba2fd2d7eca546fdbce30ae41b264479af
Mozart Al Khateeb <mozart.alkhateeb@gmail.com>
2019-12-26 18:16:19 +0200

    Clean up project, Add Default Empty project

---
+++ xperience/android/app/src/main/kotlin/com/ids/xperience/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ids.xperience
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
